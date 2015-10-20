371cbca63d49676f1ed2ebecc70186aa3e1a75a2
alx9r <eggy.island9@gmail.com>
2015-10-20 16:07:48 -0700

    added comment-based help

--- Functions/omreport.ps1
+++ Functions/omreport.ps1
@@ -1,15 +1,38 @@
-﻿
-
-function ConvertFrom-OmreportStream
+﻿function ConvertFrom-OmreportStream
 {
+<#
+.SYNOPSIS
+Converts the output of omreport to a rich object.
+
+.DESCRIPTION
+ConvertFrom-OmreportStream converts the output of the Dell Open Management Server Administation command line tool "omreport".  The output of omreport is a character stream.  ConvertFrom-OmreportStream outputs a rich object representing the contents of the character stream.  The objects output by ConvertFrom-OmreportStream can be manipulated using idiomatic PowerShell.
+
+The output of omreport should be in semicolon-separated-values "ssv" format.  ssv format is selected by providing the "-fmt ssv" option to omreport.  Other delimiters might work and can be specified by Delimiter.
+
+.OUTPUTS
+The object representing the contents of the omreport character stream.
+
+.EXAMPLE
+    Out-Null
+    omreport storage pdisk controller=0 -fmt ssv |
+        ConvertFrom-OmreportStream |
+        ? {$_.'Hot Spare' -eq 'Dedicated'} |
+        select ID,Capacity
+
+The code above outputs the physical ID and capacity of hard drives that are dedicated hot spares connected to controller 0.
+
+#>
     [CmdletBinding()]
+    [OutputType([pscustomobject])]
     param
     (
+        # The stream object output by omreport when invoked from PowerShell.
         [parameter(Mandatory = $true,
                    Position = 1)]
         [object[]]
         $OmreportStream,
 
+        # The character(s) used to delimit the fields in the omreport character stream.
         [string]
         $Delimiter = ';'
     )
